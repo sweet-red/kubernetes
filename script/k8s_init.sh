48ee14b9d15080f78cc04eb93adaaf9369f4db20
Your Name <you@example.com>
2021-03-05 14:35:24 +0800

    init.sh

--- script/k8s_init.sh
+++ script/k8s_init.sh
@@ -30,6 +30,13 @@ iptables_config(){
     iptables -F
     service iptables save
 }
+
+#关闭NetworkManager
+
+networdmanage_config(){
+	systemctl stop NetworkManager
+    systemctl disable NetworkManager
+}
 #关闭selinux
 selinux_config(){
     sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
@@ -60,7 +67,7 @@ time_config(){
 sysctl_config(){
 cp /etc/sysctl.conf /etc/sysctl.conf.bak
 cat > /etc/sysctl.conf << EOF
-net.ipv4.ip_forward = 0
+net.ipv4.ip_forward = 1
 net.ipv4.conf.default.rp_filter = 1
 net.ipv4.conf.default.accept_source_route = 0
 kernel.sysrq = 0
@@ -93,13 +100,18 @@ net.ipv4.tcp_fin_timeout = 1
 net.ipv4.tcp_keepalive_time = 30
 net.ipv4.ip_local_port_range = 1024 65000
 #k8s
-net.bridge.bridge-nf-call-iptables=1
-net.bridge.bridge-nf-call-ip6tables=1
-net.ipv4.ip_forward=1
-vm.swappiness=0
-vm.overcommit_memory=1
-vm.panic_on_oom=0
-fs.inotify.max_user_watches=89100
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+vm.swappiness = 0
+vm.overcommit_memory = 1
+vm.panic_on_oom = 0
+fs.inotify.max_user_watches = 89100
+fs.may_detach_mounts = 1
+fs.file-max = 52706963
+fs.nr_open = 52706963
+net.netfilter.nf_conntrack_max = 2310720
+
 EOF
 /sbin/sysctl -p
 echo "sysctl set OK!!"
@@ -160,6 +172,7 @@ main(){
     yum_config
 	yum_k8s_config
     iptables_config
+	networdmanage_config
     time_config
     selinux_config
     ulimit_config
